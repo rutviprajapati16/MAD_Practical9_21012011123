c160b37c4f31ab1c29b7f9b91c355f2d459ee68f
RUTVI <rutvi162004@gmail.com>
2023-10-07 13:45:42 +0530

    practical9

--- app/src/main/java/com/rutvi/mad_practical9_21012011123/SplashActivity.kt
+++ app/src/main/java/com/rutvi/mad_practical9_21012011123/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.rutvi.mad_practical9_21012011123
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity() {
+class Splash_Activity : AppCompatActivity() {
+    lateinit var logonanimation:AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val image:ImageView=findViewById(R.id.uvpce_logo)
+        image.setBackgroundResource(R.drawable.uvpce_animation_list)
+        logonanimation=image.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            logonanimation.start()
+        }
+        else{
+            logonanimation.stop()
+        }
     }
 }
