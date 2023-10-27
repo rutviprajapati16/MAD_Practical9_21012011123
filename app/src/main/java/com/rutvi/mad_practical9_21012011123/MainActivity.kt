2a1f686466c371ce7a6db8e3b2610fb182fbf074
RUTVI <rutvi162004@gmail.com>
2023-10-27 09:14:28 +0530

    practical9

--- app/src/main/java/com/rutvi/mad_practical9_21012011123/MainActivity.kt
+++ app/src/main/java/com/rutvi/mad_practical9_21012011123/MainActivity.kt
@@ -6,28 +6,32 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var alarmanimation:AnimationDrawable
-    lateinit var heartanimation:AnimationDrawable
+
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageView=findViewById<ImageView>(R.id.alarm)
-        imageView.setBackgroundResource(R.drawable.alarm_animation_list)
-        alarmanimation=imageView.background as AnimationDrawable
+
+        val alarm : ImageView = findViewById(R.id.alarm)
+        alarm.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = alarm.background as AnimationDrawable
+
         val heart : ImageView = findViewById(R.id.heart)
         heart.setBackgroundResource(R.drawable.heart_animation_list)
         heartanimation = heart.background as AnimationDrawable
     }
 
-
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        if (hasFocus){
+        if (hasFocus) {
             alarmanimation.start()
             heartanimation.start()
         }
-        else
+        else {
             alarmanimation.stop()
             heartanimation.stop()
+        }
     }
 }
--- app/src/main/java/com/rutvi/mad_practical9_21012011123/SplashActivity.kt
+++ app/src/main/java/com/rutvi/mad_practical9_21012011123/SplashActivity.kt
@@ -1,27 +1,52 @@
 package com.rutvi.mad_practical9_21012011123
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
-class Splash_Activity : AppCompatActivity() {
-    lateinit var logonanimation:AnimationDrawable
+class SplashActivity : AppCompatActivity() {
+    lateinit var logoanimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val image:ImageView=findViewById(R.id.uvpce_logo)
-        image.setBackgroundResource(R.drawable.uvpce_animation_list)
-        logonanimation=image.background as AnimationDrawable
+
+        val logo: ImageView = findViewById(R.id.uvpce_logo)
+
+        logo.setBackgroundResource(R.drawable.uvpce_animation_list)
+        logoanimation = logo.background as AnimationDrawable;
+
+        val myAnimation = AnimationUtils.loadAnimation(this, R.anim.twin_animation)
+        logo.startAnimation(myAnimation)
+
+        // Add an animation listener to your animation
+        myAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Animation ended, start a new activity here
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+                // Animation repeated
+            }
+        })
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        if(hasFocus){
-            logonanimation.start()
-        }
-        else{
-            logonanimation.stop()
+        if (hasFocus) {
+            logoanimation.start()
+        } else {
+            logoanimation.stop()
         }
     }
 }
